7bd3f4f618fa2d9d311fb7736ce793d1b587cb44
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-09-28 13:21:08 -0500

    28 9 2020 2

--- events/first.js
+++ events/first.js
@@ -1,3 +1,11 @@
 function hola() {
-  alert("hola");
+  document.getElementById("oiecomova").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "flex";
+  });
+}
+
+function adios() {
+  document.getElementById("mary").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+  });
 }
