43b90258d5d2db2bc0e0173375a18f9cb4c42ed1
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-09-30 09:59:04 -0500

    30 09 2020

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,28 @@
-var express = require("express");
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const knex = require("../db/knex");
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "TODO" });
+
+
+router.get("/new", function (req, res){
+  res.render("new");
+});
+
+
+
+function validTodo(todo) {
+  return typeof todo.calificacion == "0" && todo.pregunta != "";
+}
+router.post("/", function (req, res) {
+  if (validTodo(req.body)) {
+    const dodo = {
+      calificacion = req.body.calificacion,
+      respuesta = req.body.respuesta
+    }
+    knex("encuestaInbox").insert(todo)
+  } else {
+    alert("error post insert A");
+  }
 });
 
 module.exports = router;
