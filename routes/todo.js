aa32a89a5ee0fe196ff1d7423fef2cb32e793914
Markinsky <70705574+Markinsky@users.noreply.github.com>
2020-09-30 14:52:20 -0500

    30 09 2020

--- routes/todo.js
+++ routes/todo.js
@@ -4,22 +4,20 @@ const knex = require("../db/knex");
 /* GET home page. */
 
 
-router.get("/new", function (req, res){
-  res.render("new");
-});
-
 
 
 function validTodo(todo) {
   return typeof todo.calificacion == "0" && todo.pregunta != "";
 }
+
 router.post("/", function (req, res) {
+  alert("hoa");
   if (validTodo(req.body)) {
     const dodo = {
       calificacion = req.body.calificacion,
       respuesta = req.body.respuesta
     }
-    knex("encuestaInbox").insert(todo)
+    knex("encuestaInbox").insert(dodo)
   } else {
     alert("error post insert A");
   }
